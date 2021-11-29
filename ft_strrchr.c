f1dab501523373a509743ce5cb9d1a21dcc6cd3c
aavezou <alixvzou@gmail.com>
2021-11-29 19:44:26 +0100

    master

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -12,18 +12,6 @@
 
 #include "libft.h"
 
-int	ft_strlen(char *str)
-{
-	int	i;
-	
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_strrchr(const char *s, int c)
 {
 	unsigned int	i;
